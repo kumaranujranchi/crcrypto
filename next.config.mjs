38971dd7e335804c76aa8bb3c4f5c1ba1de438ca
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2025-10-07 11:58:20 +0530

    Configure for Netlify deployment: Update next.config.mjs and add _redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/crypto-nextjs' : ''
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true
   }
